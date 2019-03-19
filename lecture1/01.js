a68b7dad5d90c57d31b8ed571e63e61ead7e46e1
KathyW666 <13222024@bjtu.edu.cn>
2019-03-19 13:29:41 +0800

    JS_demo
    creat JS_demo
    from Udemy course

---
+++ lecture1/01.js
@@ -0,0 +1,2 @@
+alert(4);
+document.write("<h1> 测试 </h1>");
---
+++ lecture6/getstyle.js
@@ -0,0 +1,17 @@
+function getStyle(object, att) {
+	return window.getComputedStyle ? getComputedStyle(object)[att] : object.currentStyle[att];
+}
+
+// jQuery的封装
+// function $(id) {
+// 	return document.getElementById(id);
+// }
+
+
+function $(str) {
+	if (typeof str === 'string') {
+		return document.getElementById(str);
+	} else if (typeof str === 'function') {
+		return window.onload = str;
+	}
+}
